a0cede208b62e17dec5d5937628c8262e30f9e89
asus <asus@user>
2017-12-04 18:22:40 -0500

    PRuebaGITPozocommitarrellagdo

--- MyEvents/src/main/java/controller/myevents/ec/edu/ups/PersonaController.java
+++ MyEvents/src/main/java/controller/myevents/ec/edu/ups/PersonaController.java
@@ -24,6 +24,9 @@ import utilidades.myevents.ec.edu.ups.SessionUtils;
 @SessionScoped
 public class PersonaController {
 
+	/*
+	 * Variable para la validacion de la cedula
+	 */
 	private static final String PATTERN_EMAIL = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
             + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 	
@@ -32,10 +35,26 @@ public class PersonaController {
 	private int id;
 	private String pactual;
 
+	/*Definicion de variables para la validacion-coincidencia del numero de cedula ingresado
+	 * */
 	@NotBlank(message = "Ingrese las contrasenias")
 	private String contrasenia;
 	private String conincidencia;
 	private String Loginexiste;
+	
+	/*Varibles donde almaceno los valores de la consulta maestro-detalles
+	 * */
+	private String nusuario;
+	private String nlocal;
+	private String ndescripcion;
+	private String ncapacidad;
+	private String ncosto;
+	
+	private int idrecuprerar; //-----------agregado
+	
+	/*DEFINICION DE VARIABLES PARA LA RECUPERACION DE LA LISTA(PERSONA-LOCAL-EVENTO)
+	 * */
+	 private List<Persona> ListPerID;
 
 	@Inject
 	private PersonaDAO pdao;
@@ -48,7 +67,97 @@ public class PersonaController {
 	public void init() {
 		personas = new Persona();
 		lpersonas = listaPersonas();
+		ListPerID = new ArrayList<Persona>();
+		consultaLocalEventos();
 	}
+	
+	
+
+	public String getNusuario() {
+		return nusuario;
+	}
+
+
+
+	public void setNusuario(String nusuario) {
+		this.nusuario = nusuario;
+	}
+
+
+
+	public String getNlocal() {
+		return nlocal;
+	}
+
+
+
+	public void setNlocal(String nlocal) {
+		this.nlocal = nlocal;
+	}
+
+
+
+	public String getNdescripcion() {
+		return ndescripcion;
+	}
+
+
+
+	public void setNdescripcion(String ndescripcion) {
+		this.ndescripcion = ndescripcion;
+	}
+
+
+
+	public String getNcapacidad() {
+		return ncapacidad;
+	}
+
+
+
+	public void setNcapacidad(String ncapacidad) {
+		this.ncapacidad = ncapacidad;
+	}
+
+
+
+	public String getNcosto() {
+		return ncosto;
+	}
+
+
+
+	public void setNcosto(String ncosto) {
+		this.ncosto = ncosto;
+	}
+
+
+
+	public int getIdrecuprerar() {
+		return idrecuprerar;
+	}
+ 
+
+
+	public void setIdrecuprerar(int idrecuprerar) {
+		this.idrecuprerar = idrecuprerar;
+		loadid(idrecuprerar);
+		consultaLocalEventos();
+	}
+
+
+
+	public List<Persona> getListPerID() {
+		return ListPerID;
+	}
+
+
+
+	public void setListPerID(List<Persona> listPerID) {
+		ListPerID = listPerID;
+	}
+
+
 
 	public String getContrasenia() {
 		return contrasenia;
@@ -115,6 +224,8 @@ public class PersonaController {
 		this.myUser = myUser;
 	}
 
+	/*Creacion del objeto Persona condicinamiento segun las sentencias de validacion
+	 * */
 	public void crear() {
 		if (coincidirContrasenia() == true) {
 			if (validarCedula() == true) {
@@ -137,6 +248,9 @@ public class PersonaController {
 		}	
 	}
 
+	/*
+	 * Comparacion de los 2 campos referentes a la contrasenia, devolucion(true/false), segun sea la cedula valida o no valida respectivamente.
+	 */
 	public boolean coincidirContrasenia() {
 		if (personas.getContrasenia().equals(this.contrasenia)) {
 			return true;
@@ -145,6 +259,9 @@ public class PersonaController {
 		}
 	}
 
+	/*
+	 * Setea las variable como vacias, ocupado al momento de haber creado el usuario y dejar los h:inputText del JSF en blanco
+	 */
 	public void inicializar() {
 		personas.setCedula("");
 		personas.setApellido("");
@@ -152,6 +269,9 @@ public class PersonaController {
 		personas.setContrasenia("");
 	}
 
+	/*
+	 * Modificacion de los objetos de tipo Persona(USUARIO/ADMIN)
+	 */
 	public String modificar() {
 		if(myUser.getPerfil().equals("USUARIO")) {
 			personas.setContrasenia(pactual);
@@ -164,22 +284,33 @@ public class PersonaController {
 		}return null;
 	}
 
+	/*
+	 * Metodo leer, donde dirije a el archivo crearPersona, dado como parametro un Id
+	 */
 	public String leer(int id) {
 		personas = pdao.selectPersona(id);
 		return "crearPersona";
 	}
 
+	/*
+	 * Metodo eliminar, llama al metodo Delete de PersonaDAO, parametro Id, para eliminar un registro especifica
+	 */
 	public String eliminar(int id) {
 		pdao.deletePersona(id);
 		System.out.println("Eliminado Usuario ..:" + personas);
 		return "actualizar";
 	}
 
+	/*
+	 * Metodo listado, devuelve un objeto Listado de tipo Persona(Devuelve todas las personas)
+	 */
 	public List<Persona> listaPersonas() {
 		lpersonas = pdao.listPersonas();
 		return lpersonas;
 	}
 
+	/*Metodo util en la validacion de la cedula
+	 * */
 	public boolean validarCedula() {
 		String ced = personas.getCedula();
 		int sum_t = 0;
@@ -214,6 +345,8 @@ public class PersonaController {
 		return resultado;
 	}
 	
+	/*Metodo para la validacion de un correo electronico
+	 * */
 	public boolean validarCorreo() {
 		String email = personas.getCorreo();
 		Pattern pattern = Pattern.compile(PATTERN_EMAIL);
@@ -222,6 +355,7 @@ public class PersonaController {
 	}
 
 
+	//Metodo para cargar Datos de una persona, pasado un parametro Id especifico, navegacion hacia recuperarPersona
 	public String loadDatosEditar(int id) {
 		System.out.println("Cargando...Persona a Editar" + id);
 		personas = pdao.selectPersona(id);
@@ -229,6 +363,8 @@ public class PersonaController {
 		return "recuperaPersona";
 	}
 
+	/*inicilizar una Sesion HTTP y establecimiento de parametros en session, FacesContext acceso tanto al contexto de JSF como HTTP
+	 * */
 	public void iniciarSesion() {
 		if (pdao.login(personas.getCorreo(), personas.getContrasenia()).size() != 0) {
 			HttpSession session = SessionUtils.getSession();
@@ -268,9 +404,10 @@ public class PersonaController {
 			personas.setCorreo("");
 			personas.setContrasenia("");
 			this.Loginexiste = "El usuario o la contrasenia son incorrectos";
-
 	}
 	
+	/*Cargar datos del usuario obtenidos en session(HTTP) y su respectiva validacion
+	 * */
 	public void cargarDatosUsuario() {
 		myUser = new Persona();
 		HttpSession session = SessionUtils.getSession();
@@ -290,12 +427,16 @@ public class PersonaController {
 		}
 	}
 	
+	/*Metodo Utilizado para la eliminacion de una sesion HTTP, con su respectiva navegacion
+	 * */
 	public String cerrarSesion() {
 		HttpSession session = SessionUtils.getSession();
 		session.invalidate();
 		return "index.xhtml";
 	}
 	
+	/*Metodo Utilizado para la verificacion de la sesion establecida, con un respectivo contexto hacia la pagina de inicio 
+	 * */
 	 public void verificaSesion(){
 		 HttpSession session = SessionUtils.getSession();
 			String nusv = (String) session.getAttribute("username");
@@ -303,12 +444,37 @@ public class PersonaController {
 					System.out.println("si tiene sesion");
 					FacesContext contex = FacesContext.getCurrentInstance();
 			        try {
-						contex.getExternalContext().redirect( "sesion.jsf" );
+			        	if(myUser.getPerfil().equals("USUARIO")) {
+			        		contex.getExternalContext().redirect( "mainUser.html" );	
+			        	}else if(myUser.getPerfil().equals("ADMIN")){
+			        		contex.getExternalContext().redirect( "mainAdmin.html" );
+			        	}
+						
 					} catch (IOException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 				}
+	 }
+	 /*OBTENCION DE LA LISTA MAESTRA (PERSONA)
+	  * */
+	 public String consultaLocalEventos() {
+		 
+		 System.out.println("ID: "+idrecuprerar+" "+ "ENTRA");
+		 ListPerID = pdao.listPersonaID(idrecuprerar);
+		 for(Persona p: ListPerID) {
+			 System.out.println("CED===================================="+p.getCedula());
+			 nusuario = p.getNombre();
+			 nlocal = p.getLocales().get(0).getNombre();
+			 ndescripcion = p.getLocales().get(0).getDescripcion();
+			 ncapacidad = p.getLocales().get(0).getCapacidad();
+			 ncosto = p.getLocales().get(0).getCosto();
+		 }
+		 return null;
+	 }
+	 public void loadid(int id) {
+		 idrecuprerar=id;
+		 
 	 }
 
 }
