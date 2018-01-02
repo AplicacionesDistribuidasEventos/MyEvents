fb5aa0b924a9a828475ee8c5884915a7f2372e39
romeinga1592 <33741001+romeinga1592@users.noreply.github.com>
2018-01-02 09:25:07 -0500

    Changes for presentation in Categoria Eventos visualization in Users by rome inga

--- MyEvents/src/main/java/modelo/myevents/ec/edu/ups/CategoriaEventos.java
+++ MyEvents/src/main/java/modelo/myevents/ec/edu/ups/CategoriaEventos.java
@@ -5,6 +5,7 @@ public class CategoriaEventos {
 	private String descripcion_eve;
 	private String fecha;
 	private String costo;
+	/*Nombre de la categoria*/
 	private String categoria;
 	
 	public String getCategoria() {
@@ -14,7 +15,7 @@ public class CategoriaEventos {
 	public void setCategoria(String categoria) {
 		this.categoria = categoria;
 	}
-
+/*
 	public String getDescripcion_cat() {
 		return descripcion_cat;
 	}
@@ -22,7 +23,7 @@ public class CategoriaEventos {
 	public void setDescripcion_cat(String descripcion_cat) {
 		this.descripcion_cat = descripcion_cat;
 	}
-
+*/
 	public String getDescripcion_eve() {
 		return descripcion_eve;
 	}
@@ -46,5 +47,13 @@ public class CategoriaEventos {
 	public void setCosto(String costo) {
 		this.costo = costo;
 	}
+
+	@Override
+	public String toString() {
+		return "CategoriaEventos [descripcion_cat=" + descripcion_cat + ", descripcion_eve=" + descripcion_eve
+				+ ", fecha=" + fecha + ", costo=" + costo + ", categoria=" + categoria + "]";
+	}
+	
+	
 
 }
